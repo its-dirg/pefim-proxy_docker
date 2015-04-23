0b0b2fef39d34ccfe4bd9960def1f3d21a2ae34e
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-04-23 11:24:19 +0200

    Fix for port forwarding. Updated metadata

--- example/run.sh
+++ example/run.sh
@@ -36,11 +36,25 @@ do
         esac
 done
 
+port=8999
 image=itsdirg/pefim_proxy
 name=pefim_proxy
 
 # Check if running on mac
 if [ $(uname) = "Darwin" ]; then
+
+    port_check=$(netstat -an | grep ${port} | wc -l)
+    port_b2d=$(VBoxManage showvminfo "boot2docker-vm" --details | grep ${port} | wc -l)
+
+    if [ ${port_b2d} = 0 ]; then
+        if [ ${port_check} = 0 ]; then
+            VBoxManage controlvm "boot2docker-vm" natpf1 "${name},tcp,127.0.0.1,${port},,${port}"
+        else
+            echo "Port: " ${port} " is already used! Change port in the files (run.sh, sp_conf.py, service_conf.py). This will be better in the future..."
+            exit 1
+        fi
+    fi
+
     # Check so the boot2docker vm is running
     if [ $(boot2docker status) != "running" ]; then
         boot2docker start
@@ -67,7 +81,7 @@ ${sudo} docker run --rm=true \
     --name ${name} \
     --hostname localhost \
     -v $PWD/etc:/opt/pefim/etc \
-    -p 8999:8999 \
+    -p ${port}:${port} \
     $DOCKERARGS \
     -i -t \
     ${image}
