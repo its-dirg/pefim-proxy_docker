8799cbea6a1a2c093038852b1606d648a95032a1
Hans Hörberg <hans.horberg@umu.se>
2015-06-02 16:06:27 +0200

    Minor fixes of corrupt metadata.
    Added flag to startup script to print error.

--- example/etc/start.sh
+++ example/etc/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 make_proxy_metadata pefim_proxy_conf > pefim_proxy_conf.xml
-pefim_server pefim_proxy_conf pefim_server_conf -d -e https://localhost:8088/TestPEFIMIdP.xml
+pefim_server pefim_proxy_conf pefim_server_conf -d -e https://localhost:8088/TestPEFIMIdP.xml -pe
